79698006ac8c42b1896c30a081917d5430086ea1
Natasha Matthews <natashamatthews90@gmail.com>
2021-11-25 11:58:48 -0600

    wrote the validateInput function and started the formsubmission function

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,9 +1,17 @@
 // Write your helper functions here!
-require('isomorphic-fetch');
+require("isomorphic-fetch");
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
+function addDestinationInfo(
+  document,
+  name,
+  diameter,
+  star,
+  distance,
+  moons,
+  imageUrl
+) {
+  // Here is the HTML formatting for our mission target div.
+  /*
                 <h2>Mission Destination</h2>
                 <ol>
                     <li>Name: </li>
@@ -17,27 +25,47 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+  if (!isNaN(testInput)) {
+    return "Is a Number";
+  } else if (isNaN(testInput)) {
+    return "Not a Number";
+  } else if (testInput === "") {
+    return "Empty";
+  }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+  let form = document.querySelector("testForm");
+  form.addEventListener("submit", function (event) {
+    let pilotName = String(document.querySelector("input[name=pilotName]"));
+    let copilotName = String(document.querySelector("input[name=copilotName]"));
+    let fuelLevel = Number(document.querySelector("input[name=fuelLevel]"));
+    let cargoMass = Number(document.querySelector("input[name=cargoMass]"));
+    // Checking to make sure the form elements have values
+    if (
+      pilotName.value === "" ||
+      copilotName.value === "" ||
+      fuelLevel.value === "" ||
+      cargoMass.value === ""
+    ) {
+      alert("All fields are required!");
+      event.preventDefault();
+    }
+  });
 }
 
 async function myFetch() {
-    let planetsReturned;
+  let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+  planetsReturned = await fetch().then(function (response) {});
 
-    return planetsReturned;
+  return planetsReturned;
 }
 
-function pickPlanet(planets) {
-}
+function pickPlanet(planets) {}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
