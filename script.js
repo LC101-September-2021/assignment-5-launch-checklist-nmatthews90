1d50c808cca2034e6b7387440d0bdc14e6edc683
Natasha Matthews <natashamatthews90@gmail.com>
2021-11-26 14:25:19 -0600

    got planet info to show on the page

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ window.addEventListener("load", function () {
 
   let listedPlanets;
   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-  let listedPlanetsResponse;
+  let listedPlanetsResponse = myFetch();
   listedPlanetsResponse
     .then(function (result) {
       listedPlanets = result;
@@ -30,5 +30,15 @@ window.addEventListener("load", function () {
     .then(function () {
       console.log(listedPlanets);
       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+      pickPlanet();
+      addDestinationInfo(
+        document,
+        listedPlanets[3].name,
+        listedPlanets[3].diameter,
+        listedPlanets[3].star,
+        listedPlanets[3].distance,
+        listedPlanets[3].moons,
+        listedPlanets[3].image
+      );
     });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -11,17 +11,17 @@ function addDestinationInfo(
   imageUrl
 ) {
   // Here is the HTML formatting for our mission target div.
-  /*
-                <h2>Mission Destination</h2>
+  let missionTarget = document.getElementById("missionTarget");
+
+  missionTarget.innerHTML = `<h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">`;
 }
 
 function validateInput(testInput) {
@@ -105,7 +105,11 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 async function myFetch() {
   let planetsReturned;
 
-  planetsReturned = await fetch().then(function (response) {});
+  planetsReturned = await fetch(
+    "https://handlers.education.launchcode.org/static/planets.json"
+  ).then(function (response) {
+    return response.json();
+  });
 
   return planetsReturned;
 }
